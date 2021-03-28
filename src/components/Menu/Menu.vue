4f27c142d1b1d4461fc5c6a511929b2141d765b7
Michael Vinicius <michaelvini62@gmail.com>
2021-03-28 18:07:14 -0300

    feat: add menu component

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <section class="menu">
+            <div class="menu-elements">
+                <p>Minha conta</p>
+                <a href="">
+                    <p>Menu</p>
+                    <img src="@/assets/chevron-down-solid.svg" alt="">
+                </a>
+            </div>
+        </section>  
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style  src="./_menu.css" scoped>
+
+
