170274de8c14d90cca284113eafee607ac201fc0
Michael Vinicius <michaelvini62@gmail.com>
2021-03-28 18:06:23 -0300

    feat: add header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <section class="header">
+            <div class="header-elements">
+                <div class="info-elements">
+                    <img src="@/assets/icon-info.svg" alt="Icone de info">
+                    <p>Ajuda</p>
+                </div>
+                <img  class="logo-img" src="./../../assets/logo-querobolsa.svg" width= '180px' alt="Logo do Quero Bolsa">
+                <div class="account-elements">
+                    <img src="@/assets/user-circle-regular.svg" style="#007A8D" alt="">
+                    <p>Conta</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style src="./_header.css" scoped>
+
