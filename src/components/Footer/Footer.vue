0fe40da42745f6742567e981e215d67a6b3f910c
Michael Vinicius <michaelvini62@gmail.com>
2021-03-28 18:07:43 -0300

    feat: add footer component

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <section class="footer">
+        <div class="footer-elements">
+            <div class="footer-contato">
+                <img src="@/assets/whatsapp-white.svg" width="50px" alt="">
+                <div class="footer-contato-elements">
+                    <h3>0800 123 2222</h3>
+                    <h3>Segunda a sexta das 8h às 22h</h3>
+                </div>
+            </div>
+            <div class="footer-help">
+                <div class="footer-chat">
+                    <img src="@/assets/chat-regular.svg" width="50px" alt="">
+                    <h3>Chat</h3>
+                </div>
+                <div class="footer-email">
+                    <img src="@/assets/email-regular.svg" width="50px" alt="">
+                    <h3>E-mail</h3>
+                </div>
+                <div class="footer-ajuda">
+                    <img src="@/assets/icon-info-white.svg" width="50px" alt="Icone de info">
+                    <h3>Ajuda</h3>
+                </div>
+            </div>
+        </div>
+        <p> Feito com &hearts; pela <b>Quero Educação</b></p>
+    </section>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style src="./_footer.css" scoped>
