cba210d426f43b47ccf50565922cdd073674fe3d
Michael Vinicius <michaelvini62@gmail.com>
2021-03-28 18:08:51 -0300

    feat: add variables.css

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Menu/>
+    <router-view />
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import Footer from './components/Footer/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Header, 
+    Menu,
+    Footer
+  }
+}
+</script>
+
+<style>
+  @import './styles/variables.css';
+</style>
