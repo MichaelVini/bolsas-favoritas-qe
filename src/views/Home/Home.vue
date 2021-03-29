e41b80e912e28a79ad1f062bc0976d8fa6578099
Michael Vinicius <michaelvini62@gmail.com>
2021-03-29 20:55:41 -0300

    feat: add scholarship  modal

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,5 +1,4 @@
 <template>
-    <div>
         <section class="home">
             <div class="my-account">
                 <a href="">
@@ -20,19 +19,76 @@
                         <li id="3"><a href="">1° semestre de 2020</a></li>
                     </ul>
                 </div>
-                <div class="add-courses">
-                    <img src="@/assets/plus-circle.svg" alt="">
-                    <h2>Adicionar bolsa</h2>
-                    <p>Clique para adicionar bolsas de cursos do seu interesse</p>
+                <div @click="changeModal" class="add-courses">
+                        <img src="@/assets/plus-circle.svg" alt="">
+                        <h2>Adicionar bolsa</h2>
+                        <p>Clique para adicionar bolsas de cursos do seu interesse</p>
                 </div>
             </div>
+            <section class="modal" v-if="showModal == true">
+                <div class="modal-container">
+                    <div class="modal-dados">
+                            <button class="close-modal" @click="changeModal">X</button>
+                            <h2>Adicionar bolsa</h2>
+                            <p>Filtre e adicione as bolsas de seu interesse.</p>
+                            <div class="select-city">
+                                <span>SELECIONE SUA CIDADE</span>
+                                <select name="city" id="city">
+                                    <option value="sjc">São José dos Campos</option>
+                                    <option value="sp">São Paulo</option>
+                                    <option value="guaru">Guarulhos</option>
+                                    <option value="jacarei">Jacareí</option>
+                                </select>
+                            </div>
+                           <div class="select-scholarship">
+                               <span>SELECIONE O CURSO DE SUA PREFERÊNCIA</span>
+                                <select name="cc" id="cc">Ciência da Computação</select>
+                           </div>
+                           <div class="select-modality">
+                               <span>COMO VOCÊ QUER ESTUDAR?</span>
+                               <div class="modality">
+                                    <div class="presencial-modality">
+                                        <input type="checkbox" name="presencial">
+                                        <label for="presencial">Presencial</label>
+                                    </div>
+                                    <div class="ead-modality">
+                                        <input type="checkbox" value="ead">
+                                        <label for="ead">A distância</label>
+                                    </div>
+                               </div>
+                           </div>
+                           <div class="select-max-payment">
+                               <span>ATÉ QUANTO PODE PAGAR?</span>
+                           </div>
+                            <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                             and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                             and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </p>
+                            <button>Cancelar</button>
+                            <button>Adicionar Bolsas</button>
+                    </div>
+                </div>
+            </section>
         </section>
-    </div>
+
+
 </template>
 
 <script>
+
 export default {
-    
+    data() {
+        return{
+            showModal: Boolean
+        }
+    },
+    methods: {
+        changeModal(){
+            this.showModal = !this.showModal
+        }
+    }
 }
 </script>
 
