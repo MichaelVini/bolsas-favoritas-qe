5eec5ff49e9af1026a49b903248baaea50547a58
Michael Vinicius <michaelvini62@gmail.com>
2021-04-20 15:58:12 -0300

    refactor: change file names and add tags to responsive footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -6,26 +6,38 @@
                     <img src="@/assets/whatsapp-white.svg" width="50px" alt="">
                     <div class="footer-contato-elements">
                         <h3>0800 123 2222</h3>
-                        <h3>Segunda a sexta das 8h às 22h</h3>
+                        <p>Seg-Sex 8h-22h</p>
                     </div>
                 </div>
                 <div class="footer-help">
                     <div class="footer-chat">
                         <img src="@/assets/chat-regular.svg" width="50px" alt="">
-                        <h3>Chat</h3>
+                        <div class="footer-chat-elements">
+                            <h3 class="chat-mobile">Chat</h3>
+                            <h3 class="chat-web">Chat ao vivo</h3>
+                            <p class="chat-web">Seg-Sex 8h-22h</p>
+                        </div>
                     </div>
                     <div class="footer-email">
                         <img src="@/assets/email-regular.svg" width="50px" alt="">
-                        <h3>E-mail</h3>
+                        <div class="footer-email-elements">
+                            <h3 class="email-mobile">E-mail</h3>
+                            <h3 class="email-web">Mande um e-mail</h3>
+                            <p class="email-web">Respondemos rapidinho</p>
+                        </div>
                     </div>
                     <div class="footer-ajuda">
                         <img src="@/assets/icon-info-white.svg" width="50px" alt="Icone de info">
-                        <h3>Ajuda</h3>
+                        <div class="footer-help-elements">
+                            <h3 class="help-mobile">Ajuda</h3>
+                            <h3 class="help-web">Central de ajuda</h3>
+                            <p class="help-web">Encontre todas as respostas</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <p> Feito com &hearts; pela <b>Quero Educação</b></p>
+        <p class="footer-phrase"> Feito com &hearts; pela <b>Quero Educação</b></p>
     </section>
 </template>
 
@@ -36,4 +48,4 @@ export default {
 }
 </script>
 
-<style src="./_footer.css" scoped>
+<style src="./footer.css" scoped>
