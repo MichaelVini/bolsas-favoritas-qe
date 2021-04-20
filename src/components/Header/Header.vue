0d2fef333ebeb53348479b75d52ede6871086284
Michael Vinicius <michaelvini62@gmail.com>
2021-04-20 15:54:47 -0300

    refactor: change file names

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,25 +2,31 @@
     <div>
         <section class="header">
             <div class="header-elements">
-                <div class="info-elements">
-                    <a href="">
-                        <img src="@/assets/icon-info.svg" width="35px" alt="Icone de info">
-                        <p>Ajuda</p>
-                    </a>
-                </div>
-                <div class="contato-elements">
-                    <img src="@/assets/whatsapp.svg" width="50px" alt="">
-                    <a href="">
-                        <h3>0800 123 222</h3>
-                        <p>Envie mensagem ou ligue</p>
-                    </a>
-                </div>
-                <img  class="logo-img" src="./../../assets/logo-querobolsa.svg" width= '140px' alt="Logo do Quero Bolsa">
-                <div class="account-elements">
-                    <a href="">
-                        <img src="@/assets/user-circle-regular.svg" style="#007A8D" width="35px" alt="">
-                        <p>Conta</p>
-                    </a>
+                <div class="header-info-elements">
+                    <div class="help-elements">
+                        <div class="info-elements">
+                            <a href="">
+                                <img src="@/assets/icon-info.svg" width="35px" alt="Icone de info">
+                                <p class="help-mobile">Ajuda</p>
+                                <p class="help-web">Como funciona</p>
+                            </a>
+                        </div>
+                        <div class="contato-elements">
+                            <img src="@/assets/whatsapp.svg" width="35px" alt="">
+                            <a href="">
+                                <h3>0800 123 222</h3>
+                                <p>Envie mensagem ou ligue</p>
+                            </a>
+                        </div>    
+                    </div>
+                    <img  class="logo-img" src="./../../assets/logo-querobolsa.svg" width= '140px' alt="Logo do Quero Bolsa">
+                    <div class="account-elements">
+                        <a href="">
+                            <img src="@/assets/user-circle-regular.svg" style="#007A8D" width="35px" alt="">
+                            <p class="account-mobile">Conta</p>
+                            <p class="account-web">Nome Sobrenome</p>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
@@ -33,5 +39,5 @@ export default {
 }
 </script>
 
-<style src="./_header.css" scoped>
+<style src="./header.css" scoped>
 
--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -18,6 +18,6 @@ export default {
 }
 </script>
 
-<style  src="./_menu.css" scoped>
+<style  src="./menu.css" scoped>
 
 
