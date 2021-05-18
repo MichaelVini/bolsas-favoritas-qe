e6abfa3fe329f8537eccff7823da6c0905a9ab13
Michael Vinicius <michaelvini62@gmail.com>
2021-05-18 00:05:19 -0300

    refactor: add course card component

---
+++ src/components/AddCourseCard/AddCourseCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div @click="changeModal" class="add-courses">
+        <img class="add-courses__image" src="@/assets/plus-circle.svg" alt="">
+        <h2>Adicionar bolsa</h2>
+        <p class="add-courses__text">Clique para adicionar bolsas de cursos do seu interesse</p>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        changeModal: { type: Function },
+    }
+}
+</script>
+<style src="./addCourseCard.css" scoped>
