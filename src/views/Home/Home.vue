3995e41d2b72dc394ced65cb4997f444d3b1bac5
Michael Vinicius <michaelvini62@gmail.com>
2021-05-18 01:36:16 -0300

    refactor: remove scholarships list and add card favorite scholarships

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -13,36 +13,18 @@
                 e receba atualizações com as melhores ofertas disponíveis.
                 </p>
 
-                <Semesters @dataSemesters="dataSemesters($event)"/>
-                
-                <div class="scholarships-list">
-                    
-                    <AddCourseCard 
-                    :changeModal="changeModal"
-                    />
+                <Semesters 
+                    @dataSemesters="dataSemesters($event)"
+                />
 
-                    <div v-for="(scholarship, index) in scholarshipsFilterBySemesters"  :key="index"  class="favorite-scholarships-list">
-                        <div class="scholarships-content">
-                            <div class="scholarship-content__logo">
-                                <img :src="scholarship.university.logo_url" alt="" width="40%">
-                            </div>
-                            <div class="scholarship-content__star-rating">
-                                <img class="scholarship-content__star-rating__image" src="@/assets/star-rating.png" width="40%">
-                                <p class="scholarships-content__text"><b>{{ scholarship.university.score }}</b></p>
-                            </div>
-                            <span class="scholarships-content__span"><b>{{scholarship.course.kind}} - {{ scholarship.course.shift}}</b></span>
-                            <p class="scholarships-content__text">Início das aulas em: {{ scholarship.start_date }}</p>
-                            <p class="scholarships-content__text">Mensalidade com o Quero Bolsa:</p>
-                            <p class="scholarships-content__text"><s>{{ scholarship.full_price }}</s></p>
-                            <h3 class="scholarships-content__h3" >R$ {{ scholarship.price_with_discount }}/mês</h3>
-                            <div class="favorite-scholarships-list__buttons">
-                                <button class="btn btn--delete" @click="deleteScholarship(index)">Excluir</button>
-                                <button class="btn btn--show-offers">Ver oferta</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                 <ModalScholarships 
+                <CardFavoriteScholarships 
+                    :dataScholarships ="dataScholarships" 
+                    :favoriteScholarships="favoriteScholarships" 
+                    :semestersScholarships="semestersScholarships"
+                    @dataShowModal="dataShowModal($event)"
+                />
+
+                <ModalScholarships 
                     v-if="showModal"
                     :changeModal="changeModal"
                     :dataScholarships ="dataScholarships"
@@ -56,7 +38,7 @@
 import ModalScholarships from '@/components/ModalScholarships/ModalScholarships'
 import Semesters from '../../components/Semesters/Semesters.vue'
 import api from '@/services/api'
-import AddCourseCard from '../../components/AddCourseCard/AddCourseCard.vue'
+import CardFavoriteScholarships from '../../components/CardFavoriteScholarships/CardFavoriteScholarships.vue'
 
 export default {
     data(){
@@ -67,29 +49,10 @@ export default {
             semestersScholarships: []
         }
     },
-    computed:{
-        scholarshipsFilterBySemesters(){
-            const filterBySecondSemester2019 = this.favoriteScholarships.filter((elem) => {
-               return elem.enrollment_semester === "2019.2"  
-            })
-
-            const filterByFirstSemester2020 = this.favoriteScholarships.filter((elem) => {
-               return elem.enrollment_semester === "2020.1"  
-            })
-            
-            if(this.semestersScholarships[1] == true){
-                return filterBySecondSemester2019
-            } else if (this.semestersScholarships[2] == true) {
-                return filterByFirstSemester2020
-            } else {
-                return this.favoriteScholarships
-            }
-        }
-    },
     components: {
         ModalScholarships,
         Semesters,
-        AddCourseCard
+        CardFavoriteScholarships
     },
     methods: {
         changeModal(){
@@ -101,8 +64,8 @@ export default {
         dataFavoriteScholarships($event){
             this.favoriteScholarships = $event
         },
-        deleteScholarship(index){
-            this.favoriteScholarships.splice(index, 1);
+        dataShowModal($event){
+            this.showModal = $event
         },
         async getData() {
             await api.get('/db.json').then( response => {
